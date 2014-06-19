b2705642bfae3df7a4950094e0cc60d61160bf25
Stephen Van Dahm <code@vandahm.com>
2014-06-19 15:15:13 -0500

    Refactored JavaScript.

--- js/application.js
+++ js/application.js
@@ -1,20 +1,44 @@
-function shuffle(array) {
-  var currentIndex = array.length
-  var temporaryValue;
-  var randomIndex;
+/**
+ * Generates a function that always returns a random item from an array.
+ *
+ * Strictly speaking, it shuffles the array and returns items sequentially
+ * from the shuffled array, reshuffling the array as necessary. If it were
+ * truly random, it would appear glitchy when the same item is returned twice
+ * in a row.
+ *
+ * @param Array a
+ * @return function
+ */
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
+function InfiniteRandomIterator(a) {
+  var master = a.slice(0);
+  var clone = [];
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+  var shuffle = function(array) {
+    var currentIndex = array.length
+    var temporaryValue;
+    var randomIndex;
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
 
-  return array;
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  };
+
+  return function() {
+    if (clone.length == 0) {
+      clone = shuffle(master.slice(0));
+    }
+    return clone.shift();
+  };
 }
